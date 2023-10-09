fd76c114dccd7588654ba8241f42a13ec6ffe73c
99894 <99894@DESKTOP-K351R76>
2023-10-09 18:50:01 +0500

    DATA: Create Applicant Model & Migrate

---
+++ xChanger.Api/Brokers/Storages/StorageBroker.Applicants.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using xChanger.Api.Models.Applicants;
+
+namespace xChanger.Api.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        public DbSet<Applicant> Applicants { get; set; }
+    }
+}
---
+++ xChanger.Api/Brokers/Storages/StorageBroker.cs
@@ -0,0 +1,24 @@
+﻿using EFxceptions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace xChanger.Api.Brokers.Storages
+{
+    public partial class StorageBroker : EFxceptionsContext
+    {
+        private readonly IConfiguration configuration;
+
+        public StorageBroker(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString =
+                this.configuration.GetConnectionString("DefaultConnection");
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
