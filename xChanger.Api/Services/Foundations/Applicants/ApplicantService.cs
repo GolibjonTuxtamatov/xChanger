415fb2295020867c550e33936046a11453bea0fa
99894 <99894@DESKTOP-K351R76>
2023-10-10 19:44:08 +0500

    FOUNDATIONS: Add Applicants

---
+++ xChanger.Api/Services/Foundations/Applicants/ApplicantService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using xChanger.Api.Brokers.Storages;
+using xChanger.Api.Models.Applicants;
+
+namespace xChanger.Api.Services.Foundations.Applicants
+{
+    public class ApplicantService : IApplicantService
+    {
+        private readonly IStorageBroker storageBroker;
+
+        public ApplicantService(IStorageBroker storageBroker)
+        {
+            this.storageBroker = storageBroker;
+        }
+
+        public async ValueTask<string> AddApplicantsAsync(IFormFile excelFile)
+        {
+            
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ xChanger.Api/Services/Foundations/Applicants/IApplicantService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.Http;
+
+namespace xChanger.Api.Services.Foundations.Applicants
+{
+    public interface IApplicantService
+    {
+        ValueTask<string> AddApplicantsAsync(IFormFile file);
+    }
+}
