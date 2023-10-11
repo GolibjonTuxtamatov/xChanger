6709fea60b8a00483fcac072f823162758ba1aa3
99894 <99894@DESKTOP-K351R76>
2023-10-11 07:28:39 +0500

    ShouldAddApplicantsAsync -> FAIL

---
+++ xChanger.Api.Tests.Unit/Services/Foundations/Applicants/ApplicantServiceTests.cs
@@ -0,0 +1,46 @@
+﻿using Moq;
+using Tynamix.ObjectFiller;
+using xChanger.Api.Brokers.Loggings;
+using xChanger.Api.Brokers.Storages;
+using xChanger.Api.Models.Applicants;
+using xChanger.Api.Services.Foundations.Applicants;
+
+namespace xChanger.Api.Tests.Unit.Services.Foundations.Applicants
+{
+    public partial class ApplicantServiceTests
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly IApplicantService applicantService;
+
+        public ApplicantServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.applicantService = new ApplicantService(
+                this.storageBrokerMock.Object,
+                this.loggingBrokerMock.Object);
+        }
+
+        private IQueryable<Applicant> CreateRandomApplicants()
+        {
+            IQueryable<Applicant> applicants =
+                CreateApplicantFiller(CreateDateTimeOffset()).Create(2).AsQueryable();
+
+            return applicants;
+        }
+
+        private Filler<Applicant> CreateApplicantFiller(DateTimeOffset date)
+        {
+            var filler = new Filler<Applicant>();
+
+            filler.Setup().OnType<DateTimeOffset>().Use(date);
+
+            return filler;
+        }
+
+        private DateTimeOffset CreateDateTimeOffset() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+    }
+}
--- xChanger.Api/Services/Foundations/Applicants/ApplicantService.cs
+++ xChanger.Api/Services/Foundations/Applicants/ApplicantService.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using xChanger.Api.Brokers.Loggings;
 using xChanger.Api.Brokers.Storages;
 using xChanger.Api.Models.Applicants;
 
@@ -9,13 +10,15 @@ namespace xChanger.Api.Services.Foundations.Applicants
     public class ApplicantService : IApplicantService
     {
         private readonly IStorageBroker storageBroker;
+        private readonly ILoggingBroker loggingBroker;
 
-        public ApplicantService(IStorageBroker storageBroker)
+        public ApplicantService(IStorageBroker storageBroker,ILoggingBroker loggingBroker)
         {
             this.storageBroker = storageBroker;
+            this.loggingBroker = loggingBroker;
         }
 
         public async ValueTask<IQueryable<Applicant>> AddApplicantsAsync(List<Applicant> applicants) =>
-            await this.storageBroker.InsertApplicantsAsync(applicants);
+            throw new System.NotImplementedException();
     }
 }
